bdda70bd4cce345f26fdb0400734d51fba262452
MTAZero <buithuy3996@gmail.com>
2018-04-26 01:29:40 +0700

    save
    save

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Container, Header, Content, Card, CardItem, Text, Icon, Right } from 'native-base';
+
+import Main from './Components/MainComponent';
+
+export default class App extends Component{
+    render(){
+        return(
+            <Main />
+        );
+    }
+}
